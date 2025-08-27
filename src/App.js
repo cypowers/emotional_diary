d5ff41dc1ff2cb3a5ba2858bdee9b427c378d6fc
cypowers <cypark73@hotmail.com>
2025-08-27 15:47:50 +0900

    react router 적용 p433

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
-import { getEmotionImgById } from './util';
+import Home from './pages/Home';
+import New from './pages/New';
+import Edit from './pages/Edit';
+import Diary from './pages/Diary';
 
 
 function App() {
   return (
     <div className="App">
-      <h1>감정 일기장</h1>
-      <img src={getEmotionImgById(1)} alt="Emotion 1" />
-      <img src={getEmotionImgById(2)} alt="Emotion 2" />
-      <img src={getEmotionImgById(3)} alt="Emotion 3" />
-      <img src={getEmotionImgById(4)} alt="Emotion 4" />
-      <img src={getEmotionImgById(5)} alt="Emotion 5" />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/new' element={<New />} />
+        <Route path='/edit' element={<Edit />} />
+        <Route path='/diary' element={<Diary />} />
+      </Routes>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/new">New</Link>
+        <Link to="/edit">Edit</Link>
+        <Link to="/diary">Diary</Link>
+      </div>
     </div>
+    
   );
 }
 
